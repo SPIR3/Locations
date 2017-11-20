8826dcf3e39a26a7d998c44caa66526e5bf3198b
SPIR3 <32450745+SPIR3@users.noreply.github.com>
2017-11-20 10:04:25 +0000

    Update index.js

--- section1example/index.js
+++ section1example/index.js
@@ -31,11 +31,12 @@ function successPosition(position) {
 	//lets get some stuff out of the position object
 	var time = position.timestamp;
 	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
 	
 	//OK. Now we want to update the display with the correct values
 	$('#time').val("Recieved data at " + time);
-	$('#lattext').val("I should contain the latitude data...");
-	
+	$('#lattext').val("Lat: " + latitude);
+	$('#longtext').val("Lon: " + longitude);	
 }
 
 //called if the position is not obtained correctly
